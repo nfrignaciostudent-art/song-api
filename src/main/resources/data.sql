d4bdbac79b8d9a61a38cf7f74560bf395da4f6f8
asistaph-star <asista.ph@gmail.com>
2026-05-13 12:01:12 +0800

    feat: prepopulate h2 database with default songs

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO song (title, artist, album, genre, url) VALUES ('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', 'Rock', 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ');
+INSERT INTO song (title, artist, album, genre, url) VALUES ('Shape of You', 'Ed Sheeran', 'Divide', 'Pop', 'https://www.youtube.com/watch?v=JGwWNGJdvx8');
+INSERT INTO song (title, artist, album, genre, url) VALUES ('Blinding Lights', 'The Weeknd', 'After Hours', 'Synth-pop', 'https://www.youtube.com/watch?v=4NRXx6U8ABQ');
+INSERT INTO song (title, artist, album, genre, url) VALUES ('Hotel California', 'Eagles', 'Hotel California', 'Classic Rock', 'https://www.youtube.com/watch?v=EqPtz5qN7HM');
